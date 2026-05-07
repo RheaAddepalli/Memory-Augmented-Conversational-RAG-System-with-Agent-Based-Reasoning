39a2be5d4a24e8ddb66a1b0f96aa69143e9ee5be
RheaAddepalli <rheaaddepalli315@gmail.com>
2026-05-07 12:57:23 +0530

    Remove unnecessary tracked files

--- test_python.php
+++
@@ -1,6 +0,0 @@
-<?php
-header("Content-Type: text/plain");
-
-$cmd = "\"C:\\xampp\\htdocs\\GenAI-Doc-old\\venv_gai_old\\Scripts\\python.exe\" -c \"print('HELLO FROM APACHE')\" 2>&1";
-
-echo shell_exec($cmd);
